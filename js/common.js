8b4ad111d726746f12cdfbd528fa93c0bbf4e86b
Viktoria Zaharova <zaharova181114@gmail.com>
2026-04-06 00:25:22 +0300

    footer

--- js/common.js
+++ js/common.js
@@ -1,2 +1,10 @@
 $('[name="phone"]').mask('+7 (999) 999-99-99');
 
+// animate scroll to top
+$('.btn-top-scroll').on('click', function (e) {
+    e.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: 0
+    });
+});
